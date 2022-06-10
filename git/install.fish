4a018d8d925f5f29088ed371999cdb1a9cca9d1b
Jason Douglas <jason@step.com>
2022-06-10 14:27:57 -0700

    Update latest settings

--- git/install.fish
+++ git/install.fish
@@ -12,7 +12,7 @@ end
 if command -qs delta
 	git config --global core.pager delta
 	git config --global interactive.diffFilter 'delta --color-only'
-	git config --global delta.syntax-theme Nord
+	git config --global delta.syntax-theme Dracula
 	git config --global delta.line-numbers true
 	git config --global delta.decorations true
 end
@@ -23,14 +23,20 @@ if command -qs code
 		and git config --global mergetool.vscode.cmd "code --wait $MERGED"
 end
 
+if command -qs gh
+	abbr -a 'grv' 'gh repo view -w'
+	abbr -a 'gpv' 'gh pr view -w'
+end
+
 abbr -a gl 'git pull --prune'
 abbr -a glg "git log --graph --decorate --oneline --abbrev-commit"
 abbr -a glga "glg --all"
 abbr -a gp 'git push origin HEAD'
 abbr -a gpa 'git push origin --all'
 abbr -a gd 'git diff'
-abbr -a gc 'git commit -s'
-abbr -a gca 'git commit -sa'
+abbr -a gc 'git commit'
+abbr -a gca 'git commit -a'
+abbr -a gcn 'git commit -n'
 abbr -a gco 'git checkout'
 abbr -a gb 'git branch -v'
 abbr -a ga 'git add'
@@ -41,5 +47,5 @@ abbr -a gs 'git status -sb'
 abbr -a glnext 'git log --oneline (git describe --tags --abbrev=0 @^)..@'
 abbr -a gw 'git switch'
 abbr -a gm 'git switch (git main-branch)'
-abbr -a gms 'git switch (git main-branch); git sync'
+abbr -a gms 'git switch (git main-branch); and git sync'
 abbr -a gwc 'git switch -c'
